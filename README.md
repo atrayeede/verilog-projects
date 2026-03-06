081d909acb42afb0349ec252fa17f3bedb21c807
atrayeede <atrayeede0025@gmail.com>
2026-03-06 09:45:16 +0530

    added README.md

---
+++ README.md
@@ -0,0 +1,18 @@
+## Synthesis Results
+
+edit every data later by looking at the files.
+Tool: Cadence Genus
+
+Clock frequency: 100 MHz
+
+Area: 2450 µm²  
+Total cell count: 312 gates
+
+Timing:
+Worst Negative Slack (WNS): 0.12 ns
+Timing met for target frequency
+
+Power:
+Total Power: 0.84 mW
+Dynamic Power: 0.65 mW
+Leakage Power: 0.19 mW
---
+++ pipelined_multiplier/synth_script.tcl
@@ -0,0 +1,32 @@
+set_attribute init_lib_search_path /home/vlsi3/Downloads/scl_pdk/stdlib/fs120/liberty/lib_flow_ss
+#set_attribute library {slow.lib fast.lib typical.lib}
+set_attribute library tsl18fs120_scl_ss.lib
+set_attribute init_hdl_search_path /home/vlsi3/Desktop/rashmi_24EC8030/pipelinedMultiplier/simulation
+#set_attribute lef_library {gsclib090_macro.lef gsclib090_tech.lef}
+set_attribute information_level 6
+set myFiles [list pipe.v]
+set basename mult_piped_8x8_2sC
+set myClk clk
+set myPeriod_ps 5000
+set myInDelay_ns 1
+set myOutDelay_ns 1
+set runname synth_report
+read_hdl ${myFiles}
+elaborate ${basename}
+set clock [define_clock -period ${myPeriod_ps} -name ${myClk} [clock_ports]]
+external_delay -input $myInDelay_ns -clock ${myClk} [find / -port ports_in/*]
+external_delay -output $myOutDelay_ns -clock ${myClk} [find / -port ports_out/*]
+dc::set_clock_transition .1 $myClk
+check_design -unresolved
+report timing -lint
+set_attribute syn_generic_effort high
+set_attribute syn_map_effort high
+set_attribute syn_opt_effort high
+syn_generic
+syn_map
+syn_opt
+write_hdl >> pipe_netlist.v
+write_sdc >> pipe.sdc
+report_timing >> pipe_timing.rpt
+report_gates >> pipe_area.rpt
+report_power >> pipe_power.rpt
